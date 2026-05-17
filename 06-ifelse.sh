38991887332b24368d76c7084bc2b1452b339525
vjmengu <igroweachday@gmail.com>
2026-05-17 16:49:24 +0530

    new addition

---
+++ 06-ifelse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Enter a number and we will say if its greater than or less than the 100"
+read X
+if [ $X -ls 100 ]
+then
+    echo " The number entered is $X and its less than 100"
+else
+    echo " The number entered is $X and its greater than 100"
+fi
